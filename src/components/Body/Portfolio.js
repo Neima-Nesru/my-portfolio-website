020026cb69b8c0596a008831efcf454fafb79e5a
Neima Nesru <neimanesru34@gmail.com>
2024-07-24 20:06:22 +0300

    portfolio updated

--- src/components/Body/Portfolio.js
+++ src/components/Body/Portfolio.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
+import { FaArrowRight } from 'react-icons/fa';
 import banner from '../../images/myWorks/banner.jpg';
 import brochure from '../../images/myWorks/flyer.jpg';
 import web from '../../images/myWorks/responsive.jpg';
@@ -21,105 +21,36 @@ import packageDesign from '../../images/myWorks/package.jpg';
 import umuju from '../../images/myWorks/umuju.jpg';
 import expense from '../../images/myWorks/expense-tacker.jpg';
 
-
-
-
 const portfolioItems = [
-  {
-    id: 1,
-    image: banner,
-    title: 'Banner Design'
-    // link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 2,
-    image: UxUi,
-    title: 'Web Design'
-    // link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 3,
-    image: frontend,
-    title: 'Front End web Dev\'t',
-    // link:'https://codechats/netlify.app/'
-  },
-  {
-    id: 4,
-    image: brochure,
-    title: 'Brochure Design'
-    // link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 5,
-    image: business,
-    title: 'Businesscard Design',
-    // link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 6,
-    image: fullstack,
-    title: 'Fullstack web Dev\'t',
-    link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 7,
-    image: brochure2,
-    title: 'Brochure Design'
-    // link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 8,
-    image: abugida,
-    title: 'Banner Design'
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 9,
-    image: samcon,
-    title: 'Businesscard Design'
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 10,
-    image: HA,
-    title: 'Bussinesscard Design'
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 11,
-    image: packageDesign,
-    title: 'Package Design'
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 12,
-    image: YB,
-    title: 'Businesscard Design'
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 13,
-    image: web,
-    title: 'Responsive web Design',
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
- 
-  {
-    id: 14,
-    image: umuju,
-    title: 'Ads Design',
-    //link:'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System'
-  },
-  {
-    id: 15,
-    image: expense,
-    title: 'Front End web Dev\'t',
-    link:'https://neima-nesru.github.io/Expense-Tracker-App/'
-  }
-  
+  { id: 1, image: banner, title: 'Banner Design', category: 'graphics' },
+  { id: 2, image: UxUi, title: 'Web Design', category: 'uxui' },
+  { id: 3, image: frontend, title: 'Front End web Dev\'t', category: 'web' },
+  { id: 4, image: brochure, title: 'Brochure Design', category: 'graphics' },
+  { id: 5, image: business, title: 'Businesscard Design', category: 'graphics' },
+  { id: 6, image: fullstack, title: 'Fullstack web Dev\'t', category: 'web', link: 'https://github.com/Neima-Nesru/Mini_Projects/tree/main/PHP_Projects/Home%20Rental%20System' },
+  { id: 7, image: brochure2, title: 'Brochure Design', category: 'graphics' },
+  { id: 8, image: abugida, title: 'Banner Design', category: 'graphics' },
+  { id: 9, image: samcon, title: 'Businesscard Design', category: 'graphics' },
+  { id: 10, image: HA, title: 'Bussinesscard Design', category: 'graphics' },
+  { id: 11, image: packageDesign, title: 'Package Design', category: 'graphics' },
+  { id: 12, image: YB, title: 'Businesscard Design', category: 'graphics' },
+  { id: 13, image: web, title: 'Responsive web Design', category: 'uxui' },
+  { id: 14, image: umuju, title: 'Ads Design', category: 'graphics' },
+  { id: 15, image: expense, title: 'Front End web Dev\'t', category: 'web', link: 'https://neima-nesru.github.io/Expense-Tracker-App/' }
 ];
 
 const Portfolio = () => {
+  const [filter, setFilter] = useState('all');
+  const [visibleSkills, setVisibleSkills] = useState(3);
+
+  const filteredSkills = filter === 'all' ? portfolioItems : portfolioItems.filter(skill => skill.category === filter);
+
+  const handleShowMore = () => {
+    setVisibleSkills(prev => prev + 4);
+  };
+
+  const allSkillsVisible = visibleSkills >= filteredSkills.length;
+
   return (
     <div className='text-white px-4 sm:px-6 md:px-8'>
       <div className='text-center'>
@@ -127,12 +58,19 @@ const Portfolio = () => {
         <h3 className='mb-4 text-gray-400'>What I have done before.</h3>
       </div>
 
+      <div className="flex justify-center mt-10">
+        <p className={`mx-4 my-2 pb-1 cursor-pointer transition-all duration-300 ${filter === 'all' ? 'border-b-2 border-purple-600 ' : 'border-none'}`} onClick={() => setFilter('all')}>All</p>
+        <p className={`mx-4 my-2 pb-1 cursor-pointer transition-all duration-300 ${filter === 'graphics' ? 'border-b-2 border-purple-600 ' : 'border-none'}`} onClick={() => setFilter('graphics')}>Graphic Design</p>
+        <p className={`mx-4 my-2 pb-1 cursor-pointer transition-all duration-300 ${filter === 'uxui' ? 'border-b-2 border-purple-600 ' : 'border-none'}`} onClick={() => setFilter('uxui')}>UX/UI</p>
+        <p className={`mx-4 my-2 pb-1 cursor-pointer transition-all duration-300 ${filter === 'web' ? 'border-b-2 border-purple-600 ' : 'border-none'}`} onClick={() => setFilter('web')}>Web Development</p>
+      </div>
+
       <div className='container mx-auto'>
         <Carousel showArrows={true} infiniteLoop={true} showStatus={false} showIndicators={false} showThumbs={false} autoPlay={true} interval={5000} className='md:hidden'>
-          {portfolioItems.map(item => (
+          {filteredSkills.slice(0, visibleSkills).map(item => (
             <div key={item.id}>
-              <a href={item.link} target='_blank'>
-              <img className='w-full rounded' src={item.image} alt={item.title} />
+              <a href={item.link} target='_blank' rel='noopener noreferrer'>
+                <img className='w-full rounded' src={item.image} alt={item.title} />
               </a>
               <p className='pt-10'>{item.title}</p>
             </div>
@@ -140,16 +78,26 @@ const Portfolio = () => {
         </Carousel>
 
         <div className='hidden md:flex flex-wrap items-center justify-between'>
-          {portfolioItems.map(item => (
+          {filteredSkills.slice(0, visibleSkills).map(item => (
             <div key={item.id} className='p-5 md:w-1/3'>
-              <a href={item.link} target='_blank'>
-              <img className='w-full rounded' src={item.image} alt={item.title} />
+              <a href={item.link} target='_blank' rel='noopener noreferrer'>
+                <img className='w-full rounded' src={item.image} alt={item.title} />
               </a>
               <p className='pt-10'>{item.title}</p>
             </div>
           ))}
         </div>
       </div>
+
+      {!allSkillsVisible && filteredSkills.length > visibleSkills && (
+        
+          <button 
+            onClick={handleShowMore}
+            className="mt-12 bg-purple-900 text-center font-semibold px-4 py-2 rounded text-white"
+          >
+            See More Works <FaArrowRight className='text-white-800 inline ml-1'/>
+          </button>
+      )}
     </div>
   );
 }
